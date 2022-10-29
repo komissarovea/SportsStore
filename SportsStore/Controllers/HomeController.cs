888b9ecfe1e5baa70954387657d97b0a559c74cb
Komissarov Eugene <komissarovea@hotmail.com>
2022-10-30 02:07:27 +0300

    add pagination

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,17 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
 
-        public IActionResult Index() => View(repository.Products);
+        public IActionResult Index(int productPage = 1)
+        {
+            var products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize);
+
+            var pagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            };
+
+            var model = new ProductsListViewModel() {
+                PagingInfo = pagingInfo,
+                Products = products
+            };
+
+            return View(model);
+        }
+
     }
 }

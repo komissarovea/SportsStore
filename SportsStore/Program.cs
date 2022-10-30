9de905d5be50de6341b4f7a8e857b1c794eaaae6
Komissarov Eugene <komissarovea@hotmail.com>
2022-10-30 14:01:40 +0300

    add cart logic and tests

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -12,12 +12,15 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
 builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+app.UseSession();
 
 app.MapControllerRoute("catpage", "{category}/Page{productPage:int}",
     new { Controller = "Home", action = "Index" });

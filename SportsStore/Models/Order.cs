93c3f734842b2a2ad5a9164ff4cbf038c3ca6786
Komissarov Eugene <komissarovea@hotmail.com>
2022-10-31 18:14:14 +0300

    add order razor components

--- SportsStore/Models/Order.cs
+++ SportsStore/Models/Order.cs
@@ -33,5 +33,8 @@ namespace SportsStore.Models
         public string? Country { get; set; }
 
         public bool GiftWrap { get; set; }
+
+        [BindNever]
+        public bool Shipped { get; set; }
     }
 }
